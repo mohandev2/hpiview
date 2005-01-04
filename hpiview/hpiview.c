30b0007f802edd92c19b161bb2f66a8431a1370d
tariqx <tariqx@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-01-04 05:11:20 +0000

    Merging from hpiview_gtk branch now that the code is stable. Current code provides: o Clean build. o GTK based C code. o Discovery/Session/Single domain. o RPT/RDR view o Sensor read.

---
+++ hpiview/hpiview.c
@@ -0,0 +1,338 @@
+/*      -*- linux-c -*-
+ *
+ * Copyright (c) 2004 by Intel Corp.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *	   Denis Sadykov
+ *
+ *
+ */
+#include <config.h>
+
+#include <gtk/gtk.h>
+#include "hview_service.h"
+#include "hview_callbacks.h"
+#include "hpiview.h"
+
+	/* Create menu bar */
+
+static GtkWidget *hview_get_menubar(HviewWidgetsT *w)
+{
+
+      GtkWidget		*mbar;
+      GtkWidget		*menu;
+      GtkWidget		*item,	*sitem;
+      GtkWidget		*image;
+
+      gchar		*label,	*image_file;
+      guint		type,	number_items;
+      gpointer		callback, data;
+      int		i;
+
+      HviewMenuItemFactoryT menu_items[] = {
+	{"_Session",	NULL, NULL, NULL, HVIEW_BRANCH},
+        {"_Quit",	"exit.png", hview_quit_call, w, HVIEW_ITEM},
+        {"_Edit",	NULL, NULL, NULL, HVIEW_BRANCH},
+        {"_Clear log",	"clear.png", hview_empty_log_call, w, HVIEW_ITEM},
+	{"_Action",	NULL, NULL, NULL, HVIEW_BRANCH},
+        {"_Discover",	"discover.png", hview_discover_call, w, HVIEW_ITEM},
+        {"_Load plugin", NULL, hview_load_plugin_call, w, HVIEW_ITEM},
+        {"_Unload plugin", NULL, hview_unload_plugin_call, w, HVIEW_ITEM},
+        {"_Help",	NULL, NULL, NULL, HVIEW_BRANCH},
+        {"_About",	"info.png", hview_about_call,	NULL, HVIEW_ITEM}
+      };
+
+      number_items = sizeof(menu_items)/sizeof(menu_items[0]);
+
+      mbar = gtk_menu_bar_new();
+
+      for (i = 0; i < number_items; i++) {
+	    type = menu_items[i].type;
+	    label = menu_items[i].label;
+	    image_file = menu_items[i].image_file;
+	    callback = menu_items[i].callback;
+	    data = menu_items[i].data;
+	    switch (menu_items[i].type) {
+	      case HVIEW_BRANCH:
+		  item = gtk_menu_item_new_with_mnemonic(label);
+		  gtk_container_add(GTK_CONTAINER(mbar), item);
+		  menu = gtk_menu_new();
+		  gtk_menu_item_set_submenu(GTK_MENU_ITEM(item), menu);
+		  break;
+	      case HVIEW_ITEM:
+		  sitem = gtk_image_menu_item_new_with_mnemonic(label);
+		  if (image_file) {
+			image = create_pixmap(image_file);
+			gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM
+						            (sitem), image);
+		  }
+		  if (callback)
+			g_signal_connect(G_OBJECT(sitem), "activate",
+					 G_CALLBACK(callback),
+					 data);
+		  gtk_container_add(GTK_CONTAINER(menu), sitem);
+		  break;
+	    }
+      }
+      return mbar;
+}
+
+	/* Create toolbar */
+
+static GtkWidget *hview_get_toolbar(HviewWidgetsT *w)
+{
+      GtkWidget		*tbar;
+      GtkWidget		*iconw;
+      GtkToolItem	*titem;
+      GtkTooltips	*tooltips;
+
+      tbar = gtk_toolbar_new();
+
+      gtk_toolbar_set_orientation(GTK_TOOLBAR(tbar),
+				  GTK_ORIENTATION_HORIZONTAL);
+      gtk_toolbar_set_style(GTK_TOOLBAR(tbar), GTK_TOOLBAR_BOTH);
+
+      iconw = create_pixmap("close.png");
+      titem = gtk_tool_button_new(iconw, "Close");
+      g_signal_connect(G_OBJECT(titem), "clicked",
+		       G_CALLBACK(hview_quit_call),
+		       (gpointer) w);
+      gtk_toolbar_insert(GTK_TOOLBAR(tbar), titem, -1);
+
+      tooltips = gtk_tooltips_new();
+      gtk_tool_item_set_tooltip(titem, tooltips, "Close session", NULL);
+
+      iconw = create_pixmap("discover.png");
+      titem = gtk_tool_button_new(iconw, "Discover");
+      g_signal_connect(G_OBJECT(titem), "clicked",
+		       G_CALLBACK(hview_discover_call),
+		       (gpointer) w);
+      gtk_toolbar_insert(GTK_TOOLBAR(tbar), titem, -1);
+
+      tooltips = gtk_tooltips_new();
+      gtk_tool_item_set_tooltip(titem, tooltips, "Discover", NULL);
+
+      iconw = create_pixmap("viewmag.png");
+      w->rsitem = gtk_tool_button_new(iconw, "Read sensor");
+      g_signal_connect(G_OBJECT(w->rsitem), "clicked",
+		       G_CALLBACK(hview_read_sensor_call),
+		       (gpointer) w);
+      gtk_toolbar_insert(GTK_TOOLBAR(tbar), w->rsitem, -1);
+
+      tooltips = gtk_tooltips_new();
+      gtk_tool_item_set_tooltip(w->rsitem, tooltips, "Read Sensor", NULL);
+      gtk_widget_set_state(GTK_WIDGET(w->rsitem), GTK_STATE_INSENSITIVE);
+
+      return tbar;
+}
+
+	/* Create "tree window" */
+
+static GtkWidget *hview_get_tree_window(HviewWidgetsT *w)
+{
+      GtkWidget		*window;
+      GtkCellRenderer	*renderer;
+      GtkTreeViewColumn	*col;
+      GtkTreeSelection	*selection;
+
+      window = gtk_scrolled_window_new(NULL, NULL);
+      gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(window),
+				     GTK_POLICY_AUTOMATIC,
+				     GTK_POLICY_AUTOMATIC);
+      gtk_widget_set_size_request(GTK_WIDGET(window),
+				  HVIEW_TREE_WINDOW_WIDTH,
+				  HVIEW_TREE_WINDOW_HEIGHT);
+
+
+      w->tree_view = gtk_tree_view_new();
+
+      renderer = gtk_cell_renderer_text_new();
+      gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(w->tree_view),
+						  -1,
+						  HVIEW_DOMAIN_COLUMN_TITLE,
+						  renderer,
+						  "text", 0, NULL);
+
+      col = gtk_tree_view_get_column(GTK_TREE_VIEW(w->tree_view), 0);
+      gtk_tree_view_column_set_clickable(col, TRUE);
+
+
+      g_signal_connect(G_OBJECT(col), "clicked",
+		       G_CALLBACK(hview_tree_column_activated_call),
+		       (gpointer) w);
+
+      g_signal_connect(G_OBJECT(w->tree_view), "row-activated",
+		       G_CALLBACK(hview_tree_row_activated_call),
+		       (gpointer) w);
+
+      selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(w->tree_view));
+
+      g_signal_connect(G_OBJECT(selection), "changed",
+		       G_CALLBACK(hview_tree_row_selected_call),
+		       (gpointer) w);
+
+
+      gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(window),
+					    w->tree_view);
+      return window;
+}
+
+	/* Create "detail window" */
+
+static GtkWidget *hview_get_detail_window(HviewWidgetsT *w)
+{
+      GtkWidget		*window;
+      GtkCellRenderer	*renderer;
+      GtkTreeViewColumn	*col;
+
+      window = gtk_scrolled_window_new(NULL, NULL);
+      gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(window),
+				     GTK_POLICY_AUTOMATIC,
+				     GTK_POLICY_AUTOMATIC);
+
+      gtk_widget_set_size_request(GTK_WIDGET(window),
+				  HVIEW_DETAIL_WINDOW_WIDTH,
+				  HVIEW_DETAIL_WINDOW_HEIGHT);
+
+      w->detail_view = gtk_tree_view_new();
+      gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(w->detail_view),
+					FALSE);
+
+      renderer = gtk_cell_renderer_text_new();
+      gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(w->detail_view),
+						  -1,
+						  NULL,
+						  renderer,
+						  "text", 0, NULL);
+      col = gtk_tree_view_get_column(GTK_TREE_VIEW(w->detail_view), 0);
+      gtk_tree_view_column_set_min_width(col, 150);
+      
+
+      renderer = gtk_cell_renderer_text_new();
+      gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(w->detail_view),
+						  -1,
+						  NULL,
+						  renderer,
+						  "text", 1, NULL);
+      
+      gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(window),
+					    w->detail_view);
+      return window;
+}
+
+	/* Create "log window" */
+
+static GtkWidget *hview_get_log_window(HviewWidgetsT *w)
+{
+      GtkWidget		*window;
+      GtkTextBuffer	*buf;
+
+      window = gtk_scrolled_window_new(NULL, NULL);
+      gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(window),
+				     GTK_POLICY_AUTOMATIC,
+				     GTK_POLICY_AUTOMATIC);
+      
+      gtk_widget_set_size_request(GTK_WIDGET(window),
+				  HVIEW_LOG_WINDOW_WIDTH,
+				  HVIEW_LOG_WINDOW_HEIGHT);
+
+      w->log_view = gtk_text_view_new();
+
+      buf = gtk_text_buffer_new(NULL);
+      gtk_text_view_set_buffer(GTK_TEXT_VIEW(w->log_view), buf);
+      gtk_text_view_set_editable(GTK_TEXT_VIEW(w->log_view), FALSE);
+      gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(w->log_view), FALSE);
+      gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(window),
+					    w->log_view);
+
+      hview_print(w, HVIEW_NAME_VERSION);
+
+      return window;
+}
+
+	/* Initialization all Hpi View widgets */
+
+static void hview_init(void)
+{
+      HviewWidgetsT	widgets;
+      GtkWidget		*menubar;
+      GtkWidget		*vpaned;
+      GtkWidget		*hpaned;
+      GtkWidget		*main_vbox;
+      GtkTreeModel	*domains;
+      gchar		err[100];
+
+// Create main window ---------------------------------------------------------
+      widgets.main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+      g_signal_connect(G_OBJECT(widgets.main_window), "destroy",
+		       G_CALLBACK(gtk_main_quit),
+		       NULL);
+      gtk_window_set_title(GTK_WINDOW(widgets.main_window), HVIEW_TITLE);
+      gtk_widget_set_size_request(GTK_WIDGET(widgets.main_window),
+				  HVIEW_MAIN_WINDOW_WIDTH,
+				  HVIEW_MAIN_WINDOW_HEIGHT);
+//-----------------------------------------------------------------------------
+
+      main_vbox = gtk_vbox_new(FALSE, 1);
+      gtk_container_set_border_width(GTK_CONTAINER(main_vbox), 1);
+      gtk_container_add(GTK_CONTAINER(widgets.main_window), main_vbox);
+
+      menubar = hview_get_menubar(&widgets);
+      gtk_box_pack_start(GTK_BOX(main_vbox), menubar, FALSE, FALSE, 0);
+
+      widgets.toolbar = hview_get_toolbar(&widgets);
+      gtk_box_pack_start(GTK_BOX(main_vbox), widgets.toolbar, FALSE, FALSE, 0);
+
+      vpaned = gtk_vpaned_new();
+      hpaned = gtk_hpaned_new();
+
+// Create scrolled "tree window" and add it to horizontal paned window --------
+      widgets.tree_window = hview_get_tree_window(&widgets);
+      gtk_paned_add1(GTK_PANED(hpaned), widgets.tree_window);
+//-----------------------------------------------------------------------------
+
+// Create scrolled "detail window" and add it to hpaned window ----------------
+      widgets.detail_window = hview_get_detail_window(&widgets);
+      gtk_paned_add2(GTK_PANED(hpaned), widgets.detail_window);
+//-----------------------------------------------------------------------------
+
+// Create scrolled "log window" and add it with hpaned window to vertical paned
+      widgets.log_window = hview_get_log_window(&widgets);
+      gtk_paned_add1(GTK_PANED(vpaned), hpaned);
+      gtk_paned_add2(GTK_PANED(vpaned), widgets.log_window);
+//-----------------------------------------------------------------------------
+
+      gtk_box_pack_start(GTK_BOX(main_vbox), vpaned, TRUE, TRUE, 0);
+
+// Create statusbar------------------------------------------------------------
+      widgets.statusbar = gtk_statusbar_new();
+      gtk_box_pack_start(GTK_BOX(main_vbox), widgets.statusbar,
+			 FALSE, FALSE, 0);
+      hview_statusbar_push(&widgets, "wellcome");
+//-----------------------------------------------------------------------------
+
+      gtk_widget_show_all(widgets.main_window);
+
+}
+
+int main(int argc, char *argv[])
+{
+      gtk_init(&argc, &argv);
+
+      g_thread_init(NULL);
+      add_pixmap_directory (PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
+      
+      hview_init();
+
+      gtk_main();
+
+      return (0);
+}
+
---
+++ hpiview/voh.c
@@ -0,0 +1,600 @@
+#include "voh.h"
+
+#define VOH_ERROR(err, str, rv) if (err) \
+				sprintf(err, "%s (error code: %s)", \
+				str, vohError2String(rv));
+
+static SaHpiSessionIdT	sessionid;
+
+static void fixstr(SaHpiTextBufferT *strptr, char *outbuff)
+{
+      size_t		datalen,	len;
+      char		*str = (char *)strptr->Data;
+
+      if ((datalen = strptr->DataLength) != 0) {
+	    len = strlen(str);
+	    strncpy(outbuff, (char *)strptr->Data, datalen);
+      }
+      outbuff[datalen] = 0;
+}
+
+int voh_init(gchar *err)
+{
+      SaErrorT	rv;
+
+      rv = saHpiSessionOpen(SAHPI_UNSPECIFIED_DOMAIN_ID, &sessionid, NULL);
+	if (rv != SA_OK) {
+	      VOH_ERROR(err, "Session opening failed", rv);
+	      return -1;
+	}
+
+	/*
+	 * Resource discovery
+	 */
+	rv = saHpiDiscover(sessionid);
+	if (rv != SA_OK) {
+	      VOH_ERROR(err, "Discovering failed", rv);
+	      return -1;
+	}
+
+	return 0;
+}
+
+int voh_fini(gchar *err)
+{
+      SaErrorT		rv;
+
+      rv = saHpiSessionClose(sessionid);
+      if (rv != SA_OK) {
+	    if (err)
+		  sprintf(err, "Session close failed (%s)",
+			  vohError2String(rv));
+	    return -1;
+      }
+
+      return 0;
+}
+
+GtkTreeModel *voh_list_domains(gchar *err)
+{
+      GtkTreeStore	*pstore;
+      GtkTreeIter	iter,	child;
+
+      pstore = gtk_tree_store_new(VOH_LIST_NUM_COL, G_TYPE_STRING,
+				  G_TYPE_UINT, G_TYPE_UINT);
+      gtk_tree_store_append(pstore, &iter, NULL);
+      gtk_tree_store_set(pstore, &iter,
+			 VOH_LIST_COLUMN_NAME, "Domain #(single)",
+			 VOH_LIST_COLUMN_ID, SAHPI_UNSPECIFIED_DOMAIN_ID,
+			 VOH_LIST_COLUMN_TYPE, VOH_ITER_IS_DOMAIN,
+			 -1);
+
+      return GTK_TREE_MODEL(pstore);
+}
+
+GtkTreeModel *voh_domain_info(gchar *err)
+{
+      SaErrorT		rv;
+      GtkTreeStore	*info_store;
+      GtkTreeIter	iter;
+      SaHpiDomainInfoT	info;
+      SaHpiTextBufferT	strptr;
+      char		outbuff[1024];
+
+      if ((rv = saHpiDomainInfoGet(sessionid, &info)) != SA_OK) {
+	    if (err)
+		  sprintf(err, "Domain info getting failed (%s)",
+			  vohError2String(rv));
+	    return NULL;
+      }
+
+      info_store = gtk_tree_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+
+      
+      gtk_tree_store_append(info_store, &iter, NULL);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "DomainId",
+			 1, "Default",
+			 -1);
+
+      strptr = info.DomainTag;
+      fixstr(&strptr, outbuff);
+      gtk_tree_store_append(info_store, &iter, NULL);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "DomainTag",
+			 1, outbuff,
+			 -1);
+
+      return GTK_TREE_MODEL(info_store);
+}
+
+GtkTreeModel *voh_resource_info(guint id, gchar *err)
+{
+      SaErrorT		rv;
+      SaHpiRptEntryT	entry;
+      GtkTreeStore	*info_store;
+      GtkTreeIter	iter;
+      gchar		name[1024];
+
+      rv = saHpiRptEntryGetByResourceId(sessionid, (SaHpiResourceIdT)id,
+				      &entry);
+      if (rv != SA_OK) {
+	    sprintf(err, "Resource info getting failed (%s)",
+			  vohError2String(rv));
+	    return NULL;
+      }
+
+      info_store = gtk_tree_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      sprintf(name, "%d", id);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "ResourceID",
+			 1, name,
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      vohFullEntityPath2String(&(entry.ResourceEntity), name);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "Enity path",
+			 1, name,
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "Capabilities",
+			 1, vohCapabilities2String(entry.ResourceCapabilities),
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "HotSwapCapabilities",
+			 1, vohHsCapabilities2String(entry.HotSwapCapabilities),
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      fixstr(&(entry.ResourceTag), name);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "ResourceTag",
+			 1, name,
+			 -1);
+
+      return GTK_TREE_MODEL(info_store);
+}
+
+GtkTreeModel *voh_rdr_info(guint rid, guint id, gchar *err)
+{
+      SaErrorT			rv;
+      SaHpiRdrT			rdr;
+      SaHpiEntryIdT		nextentryid;
+      SaHpiSensorRecT		*sensor;
+      GtkTreeStore		*info_store;
+      GtkTreeIter		iter,		child;
+      gchar			ids[100];
+      gchar			name[1024];
+
+      rv = saHpiRdrGet(sessionid, rid, id, &nextentryid, &rdr);
+      if (rv != SA_OK) {
+	    VOH_ERROR(err, "Rdr info getting failed", rv);
+	    return NULL;
+      }
+
+      info_store = gtk_tree_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      sprintf(ids, "%d", rdr.RecordId);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "Rdr Id",
+			 1, ids,
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "Type",
+			 1, vohRdrType2String(rdr.RdrType),
+			 -1);
+
+      gtk_tree_store_append(info_store, &iter, NULL);
+      vohFullEntityPath2String(&(rdr.Entity), name);
+      gtk_tree_store_set(info_store, &iter,
+			 0, "Entity path",
+			 1, name,
+			 -1);
+
+      switch (rdr.RdrType) {
+	case SAHPI_SENSOR_RDR:
+	    sensor = &(rdr.RdrTypeUnion.SensorRec);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    sprintf(ids, "%d", sensor->Num);
+	    gtk_tree_store_set(info_store, &iter,
+			       0, "Sensor Id",
+			       1, ids,
+			       -1);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    gtk_tree_store_set(info_store, &iter,
+				  0, "Sensor type",
+				  1, vohSensorType2String(sensor->Type),
+				  -1);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    gtk_tree_store_set(info_store, &iter,
+			       0, "Event category",
+			       1, vohEventCategory2String(sensor->Category),
+			       -1);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    gtk_tree_store_set(info_store, &iter,
+			       0, "Sensor control",
+			       1, vohBoolean2String(sensor->EnableCtrl),
+			       -1);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    gtk_tree_store_set(info_store, &iter,
+			       0, "Sensor event control",
+			       1, vohSensorEventCtrl2String(sensor->EventCtrl),
+			       -1);
+
+	    gtk_tree_store_append(info_store, &iter, NULL);
+	    gtk_tree_store_set(info_store, &iter,
+			       0, "Event states supported",
+			       -1);
+	    gtk_tree_store_append(info_store, &child, &iter);
+	    gtk_tree_store_set(info_store, &child,
+			       1, vohEventState2String(sensor->Events,
+						       sensor->Category),
+			       -1);
+
+	    if (sensor->DataFormat.IsSupported == TRUE) {
+		  gtk_tree_store_append(info_store, &iter, NULL);
+		  gtk_tree_store_set(info_store, &iter,
+				     0, "Sensor data",
+				     -1);
+
+		  gtk_tree_store_append(info_store, &child, &iter);
+		  gtk_tree_store_set(info_store, &child,
+				     0, "Sensor base unit",
+				     1, vohSensorUnits2String(
+					    sensor->DataFormat.BaseUnits),
+				     -1);
+
+		  gtk_tree_store_append(info_store, &child, &iter);
+		  gtk_tree_store_set(info_store, &child,
+				     0, "Modifier base unit",
+				     1, vohSensorUnits2String(
+					    sensor->DataFormat.ModifierUnits),
+				     -1);
+
+		  if (sensor->DataFormat.ModifierUse ==
+		      			SAHPI_SMUU_BASIC_TIMES_MODIFIER) {
+		  	sprintf(name, "%s*%s",
+				vohSensorUnits2String(
+				    sensor->DataFormat.BaseUnits),
+				vohSensorUnits2String(
+					    sensor->DataFormat.ModifierUnits));
+		  } else if (sensor->DataFormat.ModifierUse ==
+		      			SAHPI_SMUU_BASIC_OVER_MODIFIER) {
+			sprintf(name, "%s/%s",
+				vohSensorUnits2String(
+				    sensor->DataFormat.BaseUnits),
+				vohSensorUnits2String(
+					    sensor->DataFormat.ModifierUnits));
+		  } else {
+			sprintf(name, "none");
+		  }
+
+		  gtk_tree_store_append(info_store, &child, &iter);
+		  gtk_tree_store_set(info_store, &child,
+					0, "Modifier use unit",
+					1, name,
+					-1);
+
+		  if (sensor->DataFormat.Range.Flags & SAHPI_SRF_MIN) {
+		       gtk_tree_store_append(info_store, &child, &iter);
+		       sprintf(name, vohSensorValue2String(
+				         &sensor->DataFormat.Range.Min));
+		       gtk_tree_store_set(info_store, &child,
+					  0, "Min value",
+					  1, name,
+					  -1);
+		  }
+
+		  if (sensor->DataFormat.Range.Flags & SAHPI_SRF_MAX) {
+		       gtk_tree_store_append(info_store, &child, &iter);
+		       sprintf(name, vohSensorValue2String(
+				         &sensor->DataFormat.Range.Max));
+		       gtk_tree_store_set(info_store, &child,
+					  0, "Max value",
+					  1, name,
+					  -1);
+		  }
+
+		  if (sensor->DataFormat.Range.Flags & SAHPI_SRF_NORMAL_MIN) {
+		       gtk_tree_store_append(info_store, &child, &iter);
+		       sprintf(name, vohSensorValue2String(
+				         &sensor->DataFormat.Range.NormalMin));
+		       gtk_tree_store_set(info_store, &child,
+					  0, "Normal Min value",
+					  1, name,
+					  -1);
+		  }
+
+		  if (sensor->DataFormat.Range.Flags & SAHPI_SRF_NORMAL_MAX) {
+		       gtk_tree_store_append(info_store, &child, &iter);
+		       sprintf(name, vohSensorValue2String(
+				         &sensor->DataFormat.Range.NormalMax));
+		       gtk_tree_store_set(info_store, &child,
+					  0, "Normal Max value",
+					  1, name,
+					  -1);
+		  }
+
+		  if (sensor->DataFormat.Range.Flags & SAHPI_SRF_NOMINAL) {
+		       gtk_tree_store_append(info_store, &child, &iter);
+		       sprintf(name, vohSensorValue2String(
+				         &sensor->DataFormat.Range.Nominal));
+		       gtk_tree_store_set(info_store, &child,
+					  0, "Nominal value",
+					  1, name,
+					  -1);
+		  }
+	    }
+
+	    
+	    if (sensor->ThresholdDefn.IsAccessible == TRUE) {
+		  gtk_tree_store_append(info_store, &iter, NULL);
+		  gtk_tree_store_set(info_store, &iter,
+				     0, "Thresholds",
+				     -1);
+
+		  gtk_tree_store_append(info_store, &child, &iter);
+		  gtk_tree_store_set(info_store, &child,
+				     0, "Readable thresholds",
+				     1, vohSensorThdMask2String(
+				            sensor->ThresholdDefn.ReadThold),
+				     -1);
+
+		  gtk_tree_store_append(info_store, &child, &iter);
+		  gtk_tree_store_set(info_store, &child,
+				     0, "Writable thresholds",
+				     1, vohSensorThdMask2String(
+				            sensor->ThresholdDefn.WriteThold),
+				     -1);
+	    }
+
+	    break;
+	default:
+	    break;
+      }
+
+      return GTK_TREE_MODEL(info_store);
+}
+
+gchar *voh_read_sensor(guint rid, guint id, gchar *err)
+{
+      SaErrorT			rv;
+      SaHpiRdrT			rdr;
+      SaHpiEntryIdT		nextentryid;
+      SaHpiSensorRecT		*sensor;
+      SaHpiSensorReadingT	reading;
+      gchar			*val;
+ 
+
+      rv = saHpiRdrGet(sessionid, rid, id, &nextentryid, &rdr);
+      if (rv != SA_OK) {
+	    VOH_ERROR(err, "Rdr info getting failed", rv);
+	    return NULL;
+      }
+
+      if (rdr.RdrType != SAHPI_SENSOR_RDR) {
+	    VOH_ERROR(err, "Rdr entry isn't sensor", -1);
+	    return NULL;
+      }
+
+      sensor = &(rdr.RdrTypeUnion.SensorRec);
+
+      rv = saHpiSensorReadingGet(sessionid, (SaHpiResourceIdT)rid,
+				 sensor->Num, &reading, NULL);
+      if (rv != SA_OK) {
+	    VOH_ERROR(err, "reading sensor failed", rv);
+	    return NULL;
+      }
+
+      val = g_malloc0(100);
+      val = g_strconcat(vohSensorValue2String(&reading), " (",
+			vohSensorUnits2Short(sensor->DataFormat.BaseUnits),
+			")", NULL);
+
+      return val;
+}
+
+GtkTreeModel *voh_list_resources(gchar *err)
+{
+      GtkTreeStore	*pstore;
+      SaHpiEntryIdT	rptentryid, nextrptentryid;
+      SaErrorT		ret;
+      SaHpiRptEntryT	rptentry;
+
+      pstore = gtk_tree_store_new(VOH_LIST_NUM_COL, G_TYPE_STRING,
+				  G_TYPE_UINT, G_TYPE_UINT);
+
+      rptentryid = SAHPI_FIRST_ENTRY;
+      while (rptentryid != SAHPI_LAST_ENTRY) {
+	    ret = saHpiRptEntryGet(sessionid, rptentryid, &nextrptentryid,
+				   &rptentry);
+	    if (ret != SA_OK)
+		  return(NULL);
+	    voh_add_resource(pstore, &rptentry);
+	    voh_list_rdrs(pstore, rptentry.ResourceId, err);
+	    rptentryid = nextrptentryid;
+      }
+
+      return (GTK_TREE_MODEL(pstore));
+}
+
+int voh_list_rdrs(GtkTreeStore *pstore, SaHpiResourceIdT rid, gchar *err)
+{
+      SaHpiRdrT			rdr;
+      SaErrorT			ret;
+      SaHpiEntryIdT		entryid;
+      SaHpiEntryIdT		nextentryid;
+
+      entryid = SAHPI_FIRST_ENTRY;
+      while (entryid != SAHPI_LAST_ENTRY) {
+	    ret = saHpiRdrGet(sessionid, rid, entryid, &nextentryid, &rdr);
+	    if (ret != SA_OK)
+		  return(-1);
+	    voh_add_rdr(pstore, &rdr, rid);
+	    entryid = nextentryid;
+      }
+}
+
+void voh_add_resource(GtkTreeStore *pstore,
+		      SaHpiRptEntryT *rpt)
+{
+      GtkTreeIter	iter,	parent,	*found_iter;
+      gint		i;
+      guint		id,	type;
+      gboolean		res,	first = FALSE;
+      SaHpiEntityPathT	entity_path = rpt->ResourceEntity;
+      gchar		path[100];
+
+      for (i = 0; i < SAHPI_MAX_ENTITY_PATH; i++) {
+	    if (entity_path.Entry[i].EntityType == SAHPI_ENT_ROOT) {
+		  break;
+	    }
+      }
+
+      res = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(pstore), &parent);
+
+      if (res == TRUE) {
+	    for (i--; i > 0; i--) {
+		  vohEntityPath2String(&(entity_path.Entry[i]), path);
+
+		  res = find_iter_by_name(GTK_TREE_MODEL(pstore),
+						 VOH_LIST_COLUMN_NAME,
+						 path, &parent);
+		  if (res == FALSE)
+			break;
+	    }
+
+      } else {
+	    i--;
+	    first = TRUE;
+      }	    
+
+      for (i; i >= 0; i--) {
+	    vohEntityPath2String(&(entity_path.Entry[i]), path);
+	    if (first == FALSE)
+		  gtk_tree_store_append(pstore, &iter, &parent);
+	    else
+		  gtk_tree_store_append(pstore, &iter, NULL);
+	    
+	    if (i == 0) {
+		  type = VOH_ITER_IS_RPT;
+		  id = (guint) rpt->ResourceId;
+	    }
+	    else {
+		  type = VOH_ITER_IS_PATH;
+		  id = 0;
+	    }
+	    
+	    gtk_tree_store_set(pstore, &iter,
+			       VOH_LIST_COLUMN_NAME, path,
+			       VOH_LIST_COLUMN_ID, id,
+			       VOH_LIST_COLUMN_TYPE, type,
+			       -1);
+	   parent = iter;
+	   first = FALSE;
+      }
+}
+
+void voh_add_rdr(GtkTreeStore *pstore,
+		 SaHpiRdrT *rdr,
+		 SaHpiResourceIdT rid)
+{
+      gboolean		res;
+      GtkTreeIter	iter,	parent;
+      SaHpiTextBufferT	strptr;
+      gchar		name[100];
+
+      if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(pstore), &parent)
+	  							== FALSE)
+	    return;
+
+      res = find_iter_by_id(GTK_TREE_MODEL(pstore), VOH_LIST_COLUMN_ID,
+			    (guint)rid, &parent);
+      if (res != FALSE) {
+	    strptr = rdr->IdString;
+	    fixstr(&strptr, name);
+	    gtk_tree_store_append(pstore, &iter, &parent);
+	    gtk_tree_store_set(pstore, &iter,
+			       VOH_LIST_COLUMN_NAME, name,
+			       VOH_LIST_COLUMN_ID, rdr->RecordId,
+			       VOH_LIST_COLUMN_TYPE, VOH_ITER_IS_RDR,
+			       -1);
+      }
+}
+
+gboolean find_iter_by_id(GtkTreeModel *model,
+			 guint column_num,
+			 guint req_id,
+			 GtkTreeIter *iter)
+{
+      gboolean		res = TRUE;
+      guint		id;
+      GtkTreeIter	niter,	child;
+
+      while (res != FALSE) {
+	    gtk_tree_model_get(model, iter, column_num, &id, -1);
+	    if (req_id == id) {
+		  return TRUE;
+	    }
+
+	    if (gtk_tree_model_iter_children(model, &child, iter) == TRUE) {
+		  res = find_iter_by_id(model, column_num, req_id, &child);
+		  if (res == TRUE) {
+			*iter = child;
+			return TRUE;
+		  }
+	    }
+	    res = gtk_tree_model_iter_next(model, iter);
+      }
+      return FALSE;
+}
+
+
+gboolean find_iter_by_name(GtkTreeModel *model,
+			   guint column_num,
+			   const gchar *req_name,
+			   GtkTreeIter *iter)
+{
+      gboolean		res = TRUE;
+      gchar		*name;
+      GtkTreeIter	child;
+
+      if (iter == NULL)
+	    return FALSE;
+
+      while (res == TRUE) {
+	    gtk_tree_model_get(model, iter, column_num, &name, -1);
+	    if (strcmp(name, req_name) == 0) {
+		  g_free(name);
+		  return TRUE;
+	    }
+	    g_free(name);
+
+	    if (gtk_tree_model_iter_children(model, &child, iter) == TRUE) {
+		  res = find_iter_by_name(model, column_num,
+						 req_name, &child);
+		  if (res == TRUE) {
+			*iter = child;
+			return TRUE;
+		  }
+	    }
+	    res = gtk_tree_model_iter_next(model, iter);
+      }
+      return FALSE;
+}
+
